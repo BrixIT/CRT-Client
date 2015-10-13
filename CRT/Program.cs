7fa8dca9770f809ecfc44198bf19d9ea442d7b8f
Martijn Braam <martijn@brixit.nl>
2015-10-13 09:33:39 +0200

    Created menu in console

--- CRT/Program.cs
+++ CRT/Program.cs
@@ -1,21 +1,73 @@
-﻿namespace CRT
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRT
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var resolver = new ModuleResolver();
-            var reportingModules = resolver.GetReportingModules();
-            foreach(var reportingModule in reportingModules)
+            bool running = true;
+            while (running)
             {
-                System.Console.WriteLine(reportingModule.ModuleName);
-                var resultParts = reportingModule.getResult();
-                foreach(var result in resultParts)
+                Console.Clear();
+                Console.WriteLine("---=[ Computer Repair Toolkit ]=---\n");
+                Console.WriteLine(" r. Go to the report menu");
+                Console.WriteLine(" t. Go to the task menu");
+                Console.WriteLine(" q. Quit");
+                var key = Console.ReadKey();
+                switch (key.Key)
                 {
-                    System.Console.WriteLine(result.FormatConsole());
+                    case ConsoleKey.R:
+                        Reporting();
+                        break;
+
+                    case ConsoleKey.T:
+                        throw new NotImplementedException();
+
+                    case ConsoleKey.Q:
+                        running = false;
+                        break;
                 }
             }
-            System.Console.ReadLine();
+        }
+
+        static void Reporting()
+        {
+            Console.Clear();
+            Console.WriteLine("---=[ Reporting ]=---\n");
+            Console.WriteLine("Enter the number of the reports to run, seperate with comma\n");
+
+            var resolver = new ModuleResolver();
+            var reportingModules = resolver.GetReportingModules();
+            var moduleIndex = new Dictionary<int, ReportingModuleInterface>();
+            var i = 1;
+            foreach (var reportingModule in reportingModules)
+            {
+                Console.Write(i);
+                Console.Write(". ");
+                Console.WriteLine(reportingModule.ModuleName);
+                moduleIndex.Add(i, reportingModule);
+                i++;
+            }
+            Console.WriteLine();
+            Console.Write("Enter numbers: ");
+            var numbers = Console.ReadLine();
+            var choises = numbers.Replace(" ", "").Split(",;".ToCharArray());
+            Console.Clear();
+            foreach(string choise in choises)
+            {
+                var moduleId = Int32.Parse(choise);
+                ReportingModuleInterface reportingModule = moduleIndex[moduleId];
+                Console.WriteLine("---=[ " + reportingModule.ModuleName + " ]=---");
+                var resultParts = reportingModule.getResult();
+                foreach (var result in resultParts)
+                {
+                    Console.WriteLine(result.FormatConsole());
+                }
+            }
+            Console.ReadLine();
+
         }
     }
 }
